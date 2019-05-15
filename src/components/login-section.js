23e6be8adf0cfa67ba605275280ef552945435f5
Cory Nanni <cory.nanni@gmail.com>
2019-05-14 22:53:53 -0400

    removed Bootstrap's Modal CSS element from login screen, replaced with own CSS

--- src/components/login-section.js
+++ src/components/login-section.js
@@ -1,8 +1,8 @@
 
 // Libraries
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 import Modal from 'react-bootstrap/lib/Modal';
 
 // CSS
@@ -16,18 +16,10 @@ export function LoginSection(props) {
         return <Redirect to="/home" />;
     }
     return (
-        <section className="login-section"> 
-            <div className="static-modal">
-                <Modal.Dialog>
-                    <Modal.Header>
-                        <Modal.Title>Sign In to your account.</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <LoginForm />
-                        <Link to="./create-account">Or sign up to create your account...</Link>
-                    </Modal.Body>
-                </Modal.Dialog>
-            </div>
+        <section className="login-section">
+            <h2>Sign In to your account.</h2>
+            <LoginForm />
+            <Link to="./create-account">Or sign up to create your account...</Link>
         </section>
     );
 }
@@ -37,4 +29,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(LoginSection);
-    
